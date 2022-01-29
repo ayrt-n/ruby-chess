ea5c96bd23a2f762c2191cdbf3c19a1d375f3aec
ayrton <ayrton.parkinson1@gmail.com>
2022-01-29 12:13:15 -0500

    Add Coordinates#chess_to_array_index

--- lib/coordinates.rb
+++ lib/coordinates.rb
@@ -1,8 +1,22 @@
 # frozen_string_literal: true
 
-# Module for transforming chess coordinates (e.g., a8) into position in array (1)
+# Module for transforming chess coordinates (e.g., a8) into position in array (0)
+#     a    b    c    d    e    f    g    h
+# 8  [0]  [1]  [2]  [3]  [4]  [5]  [6]  [7]
+# 7  [8]  [9] [10] [11] [12] [13] [14] [15]
+# 6 [16] [17] [18] [19] [20] [21] [22] [23]
+# 5 [24] [25] [26] [27] [28] [29] [30] [31]
+# 4 [32] [33] [34] [35] [36] [37] [38] [39]
+# 3 [40] [41] [42] [43] [44] [45] [46] [47]
+# 2 [48] [49] [50] [51] [52] [53] [54] [55]
+# 1 [56] [57] [58] [59] [60] [61] [62] [63]
+
 module Coordinates
   def chess_to_array_index(chess_coord)
-    
+    x, y = chess_coord.split('')
+    x = (x.ord - 'a'.ord)
+    y = (y.to_i - 8).abs * 8
+
+    x + y
   end
 end
--- spec/coordinates_spec.rb
+++ spec/coordinates_spec.rb
@@ -1,6 +1,29 @@
 # frozen_string_literal: true
+
 require './lib/coordinates'
 
+RSpec.configure do |c|
+  c.include Coordinates
+end
+
 describe Coordinates do
-  
+  describe "#chess_to_array_index" do
+    it "converts chess coordinate into appropriate array index (0-63)" do
+      chess_coord = 'a8'
+      array_index = chess_to_array_index(chess_coord)
+      expect(array_index).to eql(0)
+    end
+
+    it "converts chess coordinate into appropriate array index (0-63)" do
+      chess_coord = 'h1'
+      array_index = chess_to_array_index(chess_coord)
+      expect(array_index).to eql(63)
+    end
+
+    it "converts chess coordinate into appropriate array index(0-63)" do
+      chess_coord = 'e5'
+      array_index = chess_to_array_index(chess_coord)
+      expect(array_index).to eql(28)
+    end
+  end
 end
