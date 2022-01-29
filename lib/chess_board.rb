ee32f68cd7cc5c7f0d5b8765204b71a52316ba24
ayrton <ayrton.parkinson1@gmail.com>
2022-01-29 11:07:53 -0500

    Create ChessBoard and spec file

---
+++ lib/chess_board.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# Class representation of chess board
+# Keeps track of and updates the position of chess pieces throughout the game
+class ChessBoard
+  def initialize(board)
+    @board = board
+  end
+end
---
+++ spec/chess_board_spec.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+require './lib/chess_board.rb'
+
+describe ChessBoard do
+  
+end
