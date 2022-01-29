c492a24b1b6670154eabc054011833968318512c
ayrton <ayrton.parkinson1@gmail.com>
2022-01-29 12:45:00 -0500

    Fix rubocop suggestions

--- spec/coordinates_spec.rb
+++ spec/coordinates_spec.rb
@@ -7,20 +7,20 @@ RSpec.configure do |c|
 end
 
 describe Coordinates do
-  describe "#chess_to_array_index" do
-    it "converts chess coordinate into appropriate array index (0-63)" do
+  describe '#chess_to_array_index' do
+    it 'converts chess coordinate into appropriate array index (0-63)' do
       chess_coord = 'a8'
       array_index = chess_to_array_index(chess_coord)
       expect(array_index).to eql(0)
     end
 
-    it "converts chess coordinate into appropriate array index (0-63)" do
+    it 'converts chess coordinate into appropriate array index (0-63)' do
       chess_coord = 'h1'
       array_index = chess_to_array_index(chess_coord)
       expect(array_index).to eql(63)
     end
 
-    it "converts chess coordinate into appropriate array index(0-63)" do
+    it 'converts chess coordinate into appropriate array index(0-63)' do
       chess_coord = 'e5'
       array_index = chess_to_array_index(chess_coord)
       expect(array_index).to eql(28)
