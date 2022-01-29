9afd02030815322674f21de13a11723c4cd7af22
ayrton <ayrton.parkinson1@gmail.com>
2022-01-29 11:25:34 -0500

    Create Coordinates module and spec

---
+++ lib/coordinates.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# Module for transforming chess coordinates (e.g., a8) into position in array (1)
+module Coordinates
+  def chess_to_array_index(chess_coord)
+    
+  end
+end
---
+++ spec/coordinates_spec.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+require './lib/coordinates'
+
+describe Coordinates do
+  
+end
