afeb838b51faaa399f5ab431405cbf22aec36919
ayrton <ayrton.parkinson1@gmail.com>
2022-01-30 20:21:28 -0500

    Add ChessPiece base class

--- lib/chess_pieces.rb
+++ lib/chess_pieces.rb
@@ -1,13 +1,19 @@
 # frozen_string_literal: true
 
 # Collection of chess piece classes
-# Each class contains logic which defines how pieces are able to move
+# Base class ChessPiece contains general chess piece behaviour and attributes
+# Subclasses contain specialized behaviour for individual pieces (e.g., how they move)
 
-# Class containing pawn play logic
-class Pawn
-  attr_reader :color
+# Base class ChessPiece which contains general chess piece behaviour
+class ChessPiece
+  attr_reader :color, :curr_position
 
-  def initialize(color:)
+  def initialize(color:, curr_position:)
     @color = color
+    @curr_position = curr_position
+
+    post_init
   end
+
+  def post_init; end
 end
