b184a57e401484d039119093244d4d93dda109d7
ayrton <ayrton.parkinson1@gmail.com>
2022-02-05 10:24:26 -0500

    Add #pretty_print

--- lib/chess_board.rb
+++ lib/chess_board.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
+require 'colorize'
+require './lib/pieces/chess_piece'
+require './lib/pieces/pawn'
+require './lib/pieces/rook'
+require './lib/pieces/bishop'
+require './lib/pieces/knight'
+require './lib/pieces/queen'
+require './lib/pieces/king'
+
 # Class representation of chess board
 # Keeps track of and updates the position of chess pieces throughout the game
 class ChessBoard
@@ -26,6 +35,23 @@ class ChessBoard
     return if board.empty?(position)
 
     board[position].color
+  end
+
+  def pretty_print
+    puts ''
+    board.each_with_index do |sq, index|
+      print " #{8 - (index / 8)} " if (index % 8).zero?
+      pattern = (index / 8).even? ? %i[red light_black] : %i[light_black red]
+
+      if sq.nil? 
+        print '   '.colorize(:background => pattern[index % 2])
+      else
+        print " #{sq} ".colorize(:background => pattern[index % 2])
+      end
+
+      puts '' if ((index + 1) % 8).zero?
+    end
+    puts '    a  b  c  d  e  f  g  h '
   end
 
   private
