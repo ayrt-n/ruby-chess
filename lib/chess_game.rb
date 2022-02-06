dc7819060749de6fb5c48ba31dc320ea79bf928f
ayrton <ayrton.parkinson1@gmail.com>
2022-02-06 08:58:50 -0500

    Add #select_piece

--- lib/chess_game.rb
+++ lib/chess_game.rb
@@ -7,10 +7,18 @@ require './lib/coordinates'
 class ChessGame
   include Coordinates 
 
-  def initialize(p1, p2, board = ChessBoard.new)
-    @current_player = p1
-    @other_player = p2
+  def initialize(current_player = :white, board = ChessBoard.new)
+    @current_player = current_player
     @board = board
+  end
+
+  def select_piece
+    loop do
+      position = prompt_player_move
+      return position if board.color(position) == current_player
+
+      puts 'Invalid selection - Please select one of your pieces'
+    end
   end
 
   def prompt_player_move
