c42176495670cca0a9513def1899a07bae96d37b
ayrton <ayrton.parkinson1@gmail.com>
2022-02-06 10:16:15 -0500

    Add simple #player_turn, will need to add ability to change move

--- lib/chess_game.rb
+++ lib/chess_game.rb
@@ -5,18 +5,29 @@ require './lib/coordinates'
 
 # Class containing game flow logic
 class ChessGame
-  include Coordinates 
+  include Coordinates
+
+  attr_reader :board
+  attr_accessor :current_player
 
   def initialize(current_player = :white, board = ChessBoard.new)
     @current_player = current_player
     @board = board
   end
 
+  def player_turn
+    board.pretty_print
+    piece = select_piece
+    move = select_move(piece)
+    board.move(piece, move)
+  end
+
   def select_move(piece)
-    valid_moves = board.board[piece].valid_moves
+    valid_moves = board.board[piece].valid_moves(board, piece)
+    board.pretty_print([piece, valid_moves].flatten)
 
     loop do
-      move = prompt_player_move
+      move = chess_to_array_index(prompt_player_move)
       return move if valid_moves.include?(move)
 
       puts 'Invalid move - Please select a valid move'
