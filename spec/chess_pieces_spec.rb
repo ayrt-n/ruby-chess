dc8fbb5fbe0b8c73057c7f2399b47854b8d6cc0c
ayrton <ayrton.parkinson1@gmail.com>
2022-02-07 06:55:48 -0500

    Refactor Bishop to use array for movement

--- spec/chess_pieces_spec.rb
+++ spec/chess_pieces_spec.rb
@@ -21,6 +21,49 @@ describe ChessPiece do
   end
 end
 
+describe Bishop do
+  describe '#valid_moves' do
+    it 'returns array of valid moves' do
+      board = double('board')
+      bishop = Bishop.new(:white)
+      allow(bishop).to receive(:move_valid?).and_return(true, false, true, false, true, false, true, false)
+      allow(bishop).to receive(:take?).and_return(false)
+      current_position = [4, 4]
+
+      valid_moves = bishop.valid_moves(board, current_position)
+
+      expect(valid_moves).to contain_exactly([3, 3], [3, 5], [5, 3], [5, 5])
+    end
+
+    context 'when there are no valid moves' do
+      it 'returns an empty array' do
+        board = double('board')
+        bishop = Bishop.new(:white)
+        allow(bishop).to receive(:move_valid?).and_return(false)
+        current_position = [4, 4]
+
+        valid_moves = bishop.valid_moves(board, current_position)
+
+        expect(valid_moves).to be_empty
+      end
+    end
+
+    context 'when bishop is blocked by enemy pieces' do
+      it 'returns array of valid moves' do
+        board = double('board')
+        bishop = Bishop.new(:white)
+        allow(bishop).to receive(:move_valid?).and_return(true)
+        allow(bishop).to receive(:take?).and_return(true)
+        current_position = [4, 4]
+
+        valid_moves = bishop.valid_moves(board, current_position)
+
+        expect(valid_moves).to contain_exactly([3, 3], [3, 5], [5, 3], [5, 5])
+      end
+    end
+  end
+end
+
 # describe Pawn do
 #   describe '#valid_moves' do
 #     context 'when pawn has not moved and cannot take' do
@@ -148,49 +191,6 @@ end
 #         valid_moves = rook.valid_moves(board, current_position)
 
 #         expect(valid_moves).to contain_exactly(28, 35, 37, 44)
-#       end
-#     end
-#   end
-# end
-
-# describe Bishop do
-#   describe '#valid_moves' do
-#     it 'returns array of valid moves' do
-#       board = double('board')
-#       bishop = Bishop.new('W')
-#       allow(bishop).to receive(:move_valid?).and_return(true, false, true, false, true, false, true, false)
-#       allow(bishop).to receive(:take?).and_return(false)
-#       current_position = 36
-
-#       valid_moves = bishop.valid_moves(board, current_position)
-
-#       expect(valid_moves).to contain_exactly(27, 29, 43, 45)
-#     end
-
-#     context 'when there are no valid moves' do
-#       it 'returns an empty array' do
-#         board = double('board')
-#         bishop = Bishop.new('W')
-#         allow(bishop).to receive(:move_valid?).and_return(false)
-#         current_position = 36
-
-#         valid_moves = bishop.valid_moves(board, current_position)
-
-#         expect(valid_moves).to be_empty
-#       end
-#     end
-
-#     context 'when bishop is blocked by enemy pieces' do
-#       it 'returns array of valid moves' do
-#         board = double('board')
-#         bishop = Bishop.new('W')
-#         allow(bishop).to receive(:move_valid?).and_return(true)
-#         allow(bishop).to receive(:take?).and_return(true)
-#         current_position = 36
-
-#         valid_moves = bishop.valid_moves(board, current_position)
-
-#         expect(valid_moves).to contain_exactly(27, 29, 43, 45)
 #       end
 #     end
 #   end
