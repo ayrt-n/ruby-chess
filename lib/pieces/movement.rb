a23221873888659a8a009d85be80c7533c31715c
ayrton <ayrton.parkinson1@gmail.com>
2022-02-07 07:17:49 -0500

    Split #up_and_down and #side_to_side

--- lib/pieces/movement.rb
+++ lib/pieces/movement.rb
@@ -66,8 +66,12 @@ module Movement
     [1, -2]
   end
 
-  def up_down_left_right
-    [up, down, left, right]
+  def up_and_down
+    [up, down]
+  end
+
+  def side_to_side
+    [left, right]
   end
 
   def diagonally
