b473d898af3b1711ca19e9f9a7c8bea65b5484b7
ayrton <ayrton.parkinson1@gmail.com>
2022-02-12 10:29:25 -0500

    Add option to save object as YAML

--- lib/savestate.rb
+++ lib/savestate.rb
@@ -3,20 +3,28 @@
 require 'yaml'
 
 # Module which allows you to save/load objects in YAML
-module Savestate
-  def to_yaml
-    obj = {}
-    instance_variables.map do |var|
-      obj[var] = instance_variable_get(var)
-    end
+class Savestate
+  attr_reader :dirname
 
+  def initialize(dirname = 'savestates')
+    @dirname = dirname
+  end
+
+  def mkdir
+    Dirmkdir(dirname) unless File.exist?(dirname)
+  end
+
+  def create_savestate(raw_obj)
+    filename = Time.now.strftime("%m-%d_%H-%M-%S")
+    mkdir
+    File.open("#{dirname}/#{filename}.yaml", 'w') { |f| f.write(to_yaml(raw_obj)) }
+  end
+
+  def to_yaml(raw_obj)
     YAML.dump(obj)
   end
 
-  def self.from_yaml(file)
-    obj = YAML.load(file)
-    obj.keys.each do |key|
-      instance_variable_set(key, obj[key])
-    end
+  def from_yaml(file)
+    YAML.load(file)
   end
 end
