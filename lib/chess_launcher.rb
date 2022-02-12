302e5c163988f4d5a0607882e0ef83ad9cb89148
ayrton <ayrton.parkinson1@gmail.com>
2022-02-12 12:18:56 -0500

    Add option to launch saved games through #select_saved_game

--- lib/chess_launcher.rb
+++ lib/chess_launcher.rb
@@ -1,18 +1,22 @@
 # frozen_string_literal: true
 
-<<<<<<< HEAD
 require './lib/savestate'
 require './lib/chess_game'
+require 'yaml'
 
 # Class to launch or load game of chess
 class ChessLauncher
   def play_game
     welcome_message
-    game_type = prompt_player_input(%w[1 2])
+    game_type = prompt_player_input([1, 2])
 
-    game = if game_type == '1'
+    game = if game_type == 1
              launch_new_game
-    end
+           else
+             select_saved_game
+           end
+
+           game.game
   end
 
   private
@@ -20,11 +24,31 @@ class ChessLauncher
   def launch_new_game
     ChessGame.new
   end
+
+  def select_saved_game
+    puts "\nSelect one of the saved games to play:"
+    savestates = Dir.glob("savestates/*.{yaml, YAML}")
+    print_saved_games(savestates)
+    file_idx = prompt_player_input([*1..savestates.size])
+    file = File.open(savestates[file_idx], 'r').read
+    p file
+    load_game(file)
+  end
+
+  def print_saved_games(savestates)
+    savestates.each_with_index do |file, idx|
+      puts "[#{idx + 1}] #{File.basename(file)}"
+    end
+  end
+
+  def load_game(string)
+    YAML.load(string)
+  end
   
   def prompt_player_input(acceptable_values)
     loop do
       input = gets.chomp
-      return input if acceptable_values.include?(input)
+      return input.to_i if acceptable_values.include?(input.to_i)
 
       puts 'Invalid selection - Please select one of the listed options'
     end
@@ -43,11 +67,3 @@ class ChessLauncher
 end
 
 ChessLauncher.new.play_game
-=======
-require './lib/chess_game'
-
-# Controller for starting/loading/replaying games of chess
-class ChessLauncher
-
-end
->>>>>>> game_board_functionality
