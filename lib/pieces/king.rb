827a3f619c41e264178b53ebef596283e38faf51
ayrton <ayrton.parkinson1@gmail.com>
2022-02-18 07:58:23 -0500

    Add check for castling

--- lib/pieces/king.rb
+++ lib/pieces/king.rb
@@ -15,11 +15,29 @@ class King < ChessPiece
     valid_moves
   end
 
+  def castle_moves(board, pos)
+    valid_castle_moves = []
+
+    side_to_side.each do |move|
+      next_move = move(pos, move)
+      next unless castle_move_valid?(board, next_move)
+      next_move = move(next_move, move)
+      castle_moves << next_move if castle_move_valid?(board, next_move)
+    end
+
+    valid_castle_moves
+  end
+
   def to_s
     '♚'.colorize(color)
   end
 
   private
+
+  def castle_move_valid?(board, move)
+    board.in_bounds?(move) && board.empty?(move) &&
+      !board.under_attack_at?(move, color)
+  end
 
   def king_moves
     up_and_down + side_to_side + diagonally
